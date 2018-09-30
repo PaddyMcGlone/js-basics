4adab0a09a6c1092bd97f0bd09b1111b0f3453c0
Paddy McGlone <paddymcglone@gmail.com>
2018-09-30 17:16:52 +0100

    Adding additional NodeJS call within the script

--- Index.js
+++ Index.js
@@ -1,3 +1,5 @@
 // My first JavaScript program.
-console.log('Hello world from the console.')
-alert('Hi Murphy')
+console.log('Hello world from the console');
+
+// My NodeJS command
+console.log('Hello from Node js.');
