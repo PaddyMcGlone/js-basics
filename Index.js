86b8dff19327aba38aa02e857fc78f8d93f29fff
Paddy McGlone <paddymcglone@gmail.com>
2018-10-01 19:52:31 +0100

    constants
    Adding a constant to the chalkboard.

--- Index.js
+++ Index.js
@@ -8,3 +8,9 @@ let firstName = 'Paddy';
 let lastName = 'McGlone';
 
 console.log(firstName+" "+lastName);
+
+const dob = '26-05-1984';
+console.log(dob);
+
+// You cannot change the value of a constant.
+//dob ='a new value';
