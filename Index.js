c33b7f6cfbb10f7621fe6990b532ebb3ad8a448b
Paddy McGlone <paddymcglone@gmail.com>
2018-10-01 19:44:01 +0100

    Variables
    Adding an introduction to variables.

--- Index.js
+++ Index.js
@@ -3,3 +3,8 @@ console.log('Hello world from the console');
 
 let name = 'Patrick';
 console.log(name);
+
+let firstName = 'Paddy';
+let lastName = 'McGlone';
+
+console.log(firstName+" "+lastName);
