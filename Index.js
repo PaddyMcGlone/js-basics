fb85f61762931ede1a53e02ef2c4262a5ee48034
Paddy McGlone <paddymcglone@gmail.com>
2018-10-01 20:10:34 +0100

    Adding primitative types

--- Index.js
+++ Index.js
@@ -1,16 +1,5 @@
-// My first JavaScript program.
-console.log('Hello world from the console');
-
-let name = 'Patrick';
-console.log(name);
-
-let firstName = 'Paddy';
-let lastName = 'McGlone';
-
-console.log(firstName+" "+lastName);
-
-const dob = '26-05-1984';
-console.log(dob);
-
-// You cannot change the value of a constant.
-//dob ='a new value';
+let name = 'Paddy';
+let age = 30;
+let isApproved = true;
+let firstName = undefined;
+let favouriteColour = null;
