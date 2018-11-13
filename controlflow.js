fdf1e39c1dafdf20894e53fb50cc02814892458a
Paddy McGlone <paddymcglone@gmail.com>
2018-11-13 19:37:08 +0000

    Adding the updates.

---
+++ controlflow.js
@@ -0,0 +1,11 @@
+let hour
+
+if(hour >= 6 && hour <=12){
+    console.log("Good Morning")
+}
+else if(hour >= 13 && hour <= 18){
+    console.log("Good afternoon")
+}
+else{
+    console.log("Good night")
+}
