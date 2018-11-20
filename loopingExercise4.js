147b50140bc01f472fbb0473ded4ee6522a8dab3
Paddy McGlone <paddymcglone@gmail.com>
2018-11-20 19:45:05 +0000

    Adding last naming change.

--- loopingExercise4.js
+++ loopingExercise4.js
@@ -1,13 +1,14 @@
-console.log(CheckSpeed(120));
+console.log(CheckSpeed(129));
 
 // CheckSpeed program
 function CheckSpeed(CurrentSpeed){
     const Speedlimit = 70
+    const PointsPerMph = 5
 
-    if(CurrentSpeed <= Speedlimit)
+    if(CurrentSpeed < (Speedlimit + PointsPerMph))
         return 'Ok';
 
-    const points = Math.floor((CurrentSpeed - Speedlimit) / 5);
+    const points = Math.floor((CurrentSpeed - Speedlimit) / PointsPerMph);
 
     if(points >= 12)
         return 'Licence suspended';
