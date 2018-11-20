6a6cd1a5e3344a4311ea9231afeb6ceed2c4f51e
Paddy McGlone <paddymcglone@gmail.com>
2018-11-20 20:16:21 +0000

    Refactor II
    Adding the conditional operator.

--- loopingExercise5.js
+++ loopingExercise5.js
@@ -2,9 +2,7 @@ DisplayAllOddsAndEvens(10);
 
 function DisplayAllOddsAndEvens(limit){
     for (let i = 1; i <= limit;  i++){
-        if (i % 2 == 0)
-            console.log(i, "Even");
-        else
-            console.log(i, "Odd");
+        let message = i % 2 == 0 ? "Even" : "Odd";
+        console.log(i, message);
     }
 }
