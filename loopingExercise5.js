b10f65e2f2847fc0e458fe6bf3338aae38b41f51
Paddy McGlone <paddymcglone@gmail.com>
2018-11-20 20:13:02 +0000

    Refactor

--- loopingExercise5.js
+++ loopingExercise5.js
@@ -1,12 +1,10 @@
-let numberLimit = 10;
-
-DisplayAllOddsAndEvens(numberLimit);
+DisplayAllOddsAndEvens(10);
 
 function DisplayAllOddsAndEvens(limit){
     for (let i = 1; i <= limit;  i++){
-        if ((i % 2) == 0)
-            console.log("EVEN", i);
+        if (i % 2 == 0)
+            console.log(i, "Even");
         else
-            console.log("ODD", i);
+            console.log(i, "Odd");
     }
 }
