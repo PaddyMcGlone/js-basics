353b29245d71c20295c16ae5e61afba29c5b91ab
Paddy McGlone <paddymcglone@gmail.com>
2018-11-21 20:17:40 +0000

    Adding loopingExercise6.js
    Adding the looping code.

---
+++ loopingExercise6.js
@@ -0,0 +1,11 @@
+const values = [0, null, undefined, '', 2 ,3];
+
+console.log(countThruthyValues(values));
+
+function countThruthyValues(values) {
+    let total = 0;
+    for(let value of values){         
+        if(value != '' ) total++;
+    }
+    console.log(total);
+}
