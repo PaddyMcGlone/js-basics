79e3d8d25dde407739aec43ff291430722188b22
Paddy McGlone <paddymcglone@gmail.com>
2018-11-27 20:44:23 +0000

    loopingExercise9 - Final refactor
    Introducing seperation of concerns,

--- loopingExercise9.js
+++ loopingExercise9.js
@@ -3,18 +3,19 @@ const marks = [80, 80, 50];
 console.log(CalculateGrades(marks))
 
 function CalculateGrades(marks){
+    let average = CalculateAverage(marks);
+
+    if(average < 60) return "F";
+    if(average < 70) return "D";    
+    if(average < 80) return "C";
+    if(average < 90) return "B"
+    return "A";    
+}
+
+function CalculateAverage(array){
     let total = 0;
-    for (const mark of marks) {
-        total += mark;
+    for (const item of array) {
+        total += item;        
     }
-
-    let average = total / marks.length;
-
-    if(average > 60) return "F";
-    if(average > 70) return "D";    
-    if(average > 80) return "C";
-    if(average > 90) return "B"
-
-    return "A";
-    
+    return total / array.length;
 }
