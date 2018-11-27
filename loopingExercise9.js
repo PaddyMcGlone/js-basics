9b228e57c1d8e1c295dbe0be0727c9389c43d64c
Paddy McGlone <paddymcglone@gmail.com>
2018-11-27 20:30:39 +0000

    loopingExercise9 - Refactor [1]

--- loopingExercise9.js
+++ loopingExercise9.js
@@ -4,25 +4,17 @@ console.log(CalculateGrades(marks))
 
 function CalculateGrades(marks){
     let total = 0;
-    for (const i of marks) {
-        total += i;
+    for (const mark of marks) {
+        total += mark;
     }
 
     let average = total / marks.length;
 
-    if(average >= 50 && average <= 59)
-        console.log(average, "F")
+    if(average > 60) return "F";
+    if(average > 70) return "D";    
+    if(average > 80) return "C";
+    if(average > 90) return "B"
 
-    if(average >= 60 && average <= 69)
-        console.log(average, "D")
-    
-    if(average >= 70 && average <= 79)
-        console.log(average, "C")
-
-    if(average >= 80 && average <= 89)
-        console.log(average, "B")
-
-    if(average >= 90)
-        console.log(average, "A")
+    return "A";
     
 }
