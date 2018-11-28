6c7b06370e00ab4f95617908920ee17fc0234fa8
Paddy McGlone <paddymcglone@gmail.com>
2018-11-28 21:20:44 +0000

    Update loopingexercise10.js
    Adding the mosh solution, slightly cleaner than my approach.

--- loopingExercise10.js
+++ loopingExercise10.js
@@ -1,15 +1,26 @@
 ShowStars(10);
+Mosh_ShowStars(10);
 
 function ShowStars(rows){
-    for(let row = 1; row <= rows; row++){
-        
+    for(let row = 1; row <= rows; row++){ 
         let stars = 1
         let message = "";
-        
+
         while(stars <= row){
             message += "* "
             stars++;
         }
+
+        console.log(message);
+    }
+}
+
+function Mosh_ShowStars(stars){
+    for (let row = 1; row <= stars; row++){
+        let message = '';
+
+        for (let star = 1; star <= row; star++)
+            message += "*";
 
         console.log(message);
     }
