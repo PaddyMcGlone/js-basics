ebc6b17655d9ef36716bf2837fa8ac5f00db9f0e
Paddy McGlone <paddymcglone@gmail.com>
2018-12-03 21:05:28 +0000

    Adding nested objects

--- Objects-Basics.js
+++ Objects-Basics.js
@@ -1,11 +1,21 @@
-/* Below are highly related objects */
+/* Below are highly related objects. */
 let radius = 10;
 let x = 1;
 let y = 1;
 
-/* Can combine to form an object */
+/* Can combine to form an object. */
 const circle = {
     radius : 10,
     x :1,
     y : 1
+}
+
+/* An example of a nested object. */
+const circle2 = {
+    radius : 10,
+    definition = {
+        placeholder : 10,
+        image : true
+    },
+    display : false
 }
