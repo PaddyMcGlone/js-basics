5fe50cc86624721919b28e6ec27bd4265e44592d
Paddy McGlone <paddymcglone@gmail.com>
2019-02-10 16:43:43 +0000

    Adding the working copy of the reduce method.

--- arrays-exercise5.js
+++ arrays-exercise5.js
@@ -3,8 +3,8 @@ const numbers = [1,2,3,4,5,6,1,7,8,1];
 let results = CountOccurences(numbers, 1);
 console.log("Total count : ", results);
 
-results = CountOccurencesWithReduce(numbers, 1);
-console.log("With reduce : ", results);
+let reduceResults = CountOccurencesWithReduce(numbers, 2);
+console.log("With reduce : ", reduceResults);
 
 // 1. Simple approach.
 function CountOccurences(array, serachItem){
@@ -19,9 +19,10 @@ function CountOccurences(array, serachItem){
 }
 
 // 2. Advanced reduce method.
-function CountOccurencesWithReduce(array, serachItem){
-     let total = array.reduce((total, currentValue) => {
-        total += currentValue === serachItem ? 1 : 0;
-     });
-     return total;
+function CountOccurencesWithReduce(array, serachItem) {
+     let sum = array.reduce((total, currentValue) => {
+        return total += currentValue === serachItem ? 1 : 0;
+     }, 0);
+
+     return sum;
 }
