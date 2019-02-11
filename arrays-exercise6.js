af6b383d20457528b421530734d6ac3c1be1cbf1
Paddy McGlone <paddymcglone@gmail.com>
2019-02-11 20:11:31 +0000

    Adding Exercise 6  - reduce method approach.

--- arrays-exercise6.js
+++ arrays-exercise6.js
@@ -3,6 +3,7 @@ let maxNumber = GetMax(myArray);
 
 console.log("The max number is : ", maxNumber);
 
+// Simple approach
 function GetMax(array){
     let max = 0;
 
@@ -10,4 +11,16 @@ function GetMax(array){
         max = max < num ? num : max;
     
     return max;
+}
+
+const myArray2 = [1,2,3,4,10,5,6,7,8];
+let otherMax = GetMaxWithReduce(myArray2);
+
+console.log("Reduce methods turn : ", otherMax);
+
+// Reduce method approach
+function GetMaxWithReduce(array){
+    return array.reduce((accumulator, currentValue) => {        
+        return accumulator < currentValue ? currentValue : accumulator;
+    },0);
 }
